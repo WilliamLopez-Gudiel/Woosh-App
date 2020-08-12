b19b6c465bb48aa114467289a4a5e904c565f749
casualwilly <32225842+casualwilly@users.noreply.github.com>
2020-08-12 13:29:21 -0400

    Initial Commit

---
+++ workoutApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  workoutApp
+//
+//  Created by William Gudiel on 8/12/20.
+//  Copyright © 2020 William. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
